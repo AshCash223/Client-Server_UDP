9dea6a7061a6dac1b31937811ffadc918efd37b9
AshCash223 <ashtess0@gmail.com>
2024-09-11 21:30:29 +1200

    Added logic to the `run()` method to parse incoming DatagramPacket requests. havnt addressed how to send an error to the client if the file dosnt exist.

--- TftpServer.java
+++ TftpServer.java
@@ -12,6 +12,11 @@ class TftpServerWorker extends Thread
 
     private void sendfile(String filename)
     {
+        File file = new File(filename);
+        if (!file.exists()) {
+            //send an error for a file not being found for the client
+            return;
+        }
         /*
          * open the file using a FileInputStream and send it, one block at
          * a time, to the receiver.
@@ -21,46 +26,58 @@ class TftpServerWorker extends Thread
 
     public void run()
     {
-        /*
-         * parse the request packet, ensuring that it is a RRQ
-         * and then call sendfile
-         */
+
+        try {
+            byte[] requestData = req.getData();
+            if (requestData[0] == 0 && requestData[1] == RRQ) { // Check if it's an RRQ using the first 2 bytes
+                String filename = new String(requestData, 2, req.getLength() - 2); //making a string constructor extracting file at index 2
+                System.out.println("Received RRQ for file: " + filename);
+                sendfile(filename);
+            } else {
+                System.out.println("Invalid request received.");
+                //sending the error packet to the client
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	return;
     }
 
     public TftpServerWorker(DatagramPacket req)
     {
-	this.req = req;
+	    this.req = req;
+        start(); //calling start instead of run directly to create a new thread
     }
 }
 
-class TftpServer
-{
+class TftpServer{
+        
     public void start_server()
     {
-	try {
-	    DatagramSocket ds = new DatagramSocket();
-	    System.out.println("TftpServer on port " + ds.getLocalPort());
+        try {
+            DatagramSocket ds = new DatagramSocket();
+            System.out.println("TftpServer on port " + ds.getLocalPort()); //gets local port number for the datagram socket
 
-	    for(;;) {
-		byte[] buf = new byte[1472];
-		DatagramPacket p = new DatagramPacket(buf, 1472);
-		ds.receive(p);
-		
-		TftpServerWorker worker = new TftpServerWorker(p);
-		worker.start();
-	    }
-	}
-	catch(Exception e) {
-	    System.err.println("Exception: " + e);
-	}
+            for(;;) {
+            byte[] buf = new byte[1472]; //creating a byte array with a byte limit of 1472 bytes
+            DatagramPacket p = new DatagramPacket(buf, 1472); //creating a datagram packet with the contents of buf(byte array) alongside max number of bytes
+            ds.receive(p); //send the datagram packet to be recived by the datagram socket
+            
+            TftpServerWorker worker = new TftpServerWorker(p); //calling the TftpServerWorker class and parsing the datagram packet on to it
+            worker.start(); //starting the code into the TftpServerWorker
 
-	return;
+            }
+        }
+        catch(Exception e) { //display error message if an exception is caught
+            System.err.println("Exception: " + e);
+        }
+
+        return;
     }
 
-    public static void main(String args[])
-    {
-	TftpServer d = new TftpServer();
-	d.start_server();
-    }
+            public static void main(String args[])
+            {
+            TftpServer d = new TftpServer();
+            d.start_server();
+            }
 }
