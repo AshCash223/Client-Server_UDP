bffce33f4e27478ab6acc7e5f72a2dd0e911d4cd
AshCash223 <ashtess0@gmail.com>
2024-09-16 15:19:43 +1200

    bug fixes, applied indentation and added comments.
    Bug fixed was last packet was being sent with 512 bytes when its less than 512

--- Client/TftpClient.java
+++ Client/TftpClient.java
@@ -22,7 +22,7 @@ public class TftpClient {
             DatagramSocket cs = new DatagramSocket(); // clients datagram socket
             InetAddress ia = InetAddress.getByName(hostName);
 
-            byte[] rrq = new byte[fileName.length()+ 1]; // byte array for the requset packet
+            byte[] rrq = new byte[fileName.length() + 1]; // byte array for the request packet
             rrq[0] = 1; // converting the file name to bytes
 
             // Convert the file name to a byte array
@@ -35,32 +35,34 @@ public class TftpClient {
             cs.send(rp); // client server sends the rrq packet
 
             int expectedBN = 1; // expected block number
-            FileOutputStream fos = new FileOutputStream(fileName);
+            FileOutputStream fos = new FileOutputStream(fileName); //file output stream to write the file
+            System.out.println("Sent request, waiting for response"); // msg to indicate request has been sent
+
+
             while (true) {
-                byte[] buffer = new byte[514]; // buffer to recieve data blocks
-                DatagramPacket response = new DatagramPacket(buffer, buffer.length); // datagram packet for recieved
-                System.out.println("Sent request, waiting for response"); //msg to indicate request has been sent
+                byte[] buffer = new byte[514]; // buffer to receive data blocks
+                DatagramPacket response = new DatagramPacket(buffer, buffer.length); // datagram packet for received
                 cs.receive(response);
 
-                byte[] recievedPacket = new byte[514]; // recievedPacket byte array to extract and
-                                   
-                recievedPacket = response.getData(); // extracts recievedPacket from response
+                byte[] recievedPacket = new byte[514]; // receivedPacket byte array to extract and
+
+                recievedPacket = response.getData(); // extracts receivedPacket from response
                 if (response.getLength() < 2) {
                     return;
                 }
 
                 int blocknumber = recievedPacket[1]; // the block number should be in the 2nd
 
-                //System.out.println(expectedBN); testing values
-                //System.out.println(blocknumber);
-                
+                // System.out.println(expectedBN); testing values
+                // System.out.println(blocknumber);
+
                 if (recievedPacket[0] == 4) { // Check if the packet is an error packet
                     // Extract the error message from the packet (starting at byte 1)
                     String errorMsg = new String(recievedPacket, 1, response.getLength() - 1);
-                    
+
                     // Print the error message
                     System.out.println("Error received: " + errorMsg);
-                    
+
                     return; // Exit or handle the error appropriately
                 }
 
@@ -69,16 +71,16 @@ public class TftpClient {
                     return;
                 }
 
-                    byte[] ackPacket =new byte[2]; // creating an ack packet to send the server
-                    ackPacket[0] = 3; // initializing the ackpackets type
-                    ackPacket[1] = (byte) blocknumber; // adding the block number to the second
-                                            
-                    DatagramPacket ackResponse = new DatagramPacket(ackPacket,ackPacket.length, ia, portNumb); // forging the ack response
+                byte[] ackPacket = new byte[2]; // creating an ack packet to send the server
+                ackPacket[0] = 3; // initializing the ackPacket's type
+                ackPacket[1] = (byte) blocknumber; // adding the block number to the second
+
+                DatagramPacket ackResponse = new DatagramPacket(ackPacket, ackPacket.length, ia, portNumb); // forging the ack response
 
                 if (expectedBN == blocknumber) {
                     fos.write(recievedPacket, 2, response.getLength() - 2);
-                    System.out.println("data packet transfered: " + recievedPacket);
-                    cs.send(ackResponse); // sending the ackresponse to the server
+                    System.out.println("transfered data packet number: " + blocknumber + " Acknowledgment sending...");
+                    cs.send(ackResponse); // sending the ackResponse to the server
                     expectedBN++;
                 }
             }
--- TftpServer.java
+++ TftpServer.java
@@ -11,88 +11,94 @@ class TftpServerWorker extends Thread
     private static final byte ACK = 3;
     private static final byte ERROR = 4;
     
-
     private void sendfile(String filename)
     {
-        try{
-        DatagramSocket ds = new DatagramSocket(); // Create a new socket for sending DATA
-        File file = new File(filename);
-        if (!file.exists()) {
-            System.err.println("file " + filename + " does not exist");
-            // Convert the string "" to a byte array
-            byte[] errorMessage = "file does not exist".getBytes(); // Convert the string to a byte array
+        try {
+            DatagramSocket ds = new DatagramSocket(); // Create a new socket for sending DATA
+            File file = new File(filename);
+            if (!file.exists()) {
+                System.err.println("file " + filename + " does not exist");
+                // Convert the string "" to a byte array
+                byte[] errorMessage = "file does not exist".getBytes(); // Convert the string to a byte array
 
-            byte[] errorPacket = new byte[1 + errorMessage.length];
+                byte[] errorPacket = new byte[1 + errorMessage.length];
 
-            errorPacket[0] = 4; // Error type byte
+                errorPacket[0] = 4; // Error type byte
 
-            // Copy the error message bytes into the errorPacket starting from index 1
-            System.arraycopy(errorMessage, 0, errorPacket, 1, errorMessage.length);
+                // Copy the error message bytes into the errorPacket starting from index 1
+                System.arraycopy(errorMessage, 0, errorPacket, 1, errorMessage.length);
 
-            // Create the DatagramPacket with the error packet and send it
-            DatagramPacket ep = new DatagramPacket(errorPacket, errorPacket.length, req.getAddress(), req.getPort()); 
-            ds.send(ep); // Send the packet
-        }
-
-        System.out.println("file exists");
-
-        try (FileInputStream fis = new FileInputStream(file)) {
-            InetAddress clientAddress = req.getAddress(); //gets the ip of the client
-            int clientPort = req.getPort(); //gets the port numb of the client
-            byte[] buf = new byte[512];
-            int blockNumber = 1;
-            int bytesRead; //byte reader
-                
-
-            while ((bytesRead = fis.read(buf)) != -1) {
-
-                byte[] array = new byte[514]; //byte array for data packets
-                array[0] = DATA;
-                array[1] = (byte)blockNumber;
-    
-                System.arraycopy(buf, 0, array, 2, buf.length); //copies the content of buf start from 0 then coppy arrays content then add the buf.lenght
-
-               
-                DatagramPacket sendPacket = new DatagramPacket(array, array.length, clientAddress, clientPort);
-                System.out.println("Sent block #" + blockNumber); 
-                Retransmit(ds,sendPacket, blockNumber); // Send DATA packet
-                blockNumber++;
+                // Create the DatagramPacket with the error packet and send it
+                DatagramPacket ep = new DatagramPacket(errorPacket, errorPacket.length, req.getAddress(), req.getPort()); 
+                ds.send(ep); // Send the packet
             }
-            System.out.println("File transmitted");
 
-        }catch(IOException e){
-            System.err.println("Input and Output error: " + e);
+            System.out.println("file exists");
+
+            try (FileInputStream fis = new FileInputStream(file)) {
+                InetAddress clientAddress = req.getAddress(); // gets the ip of the client
+                int clientPort = req.getPort(); // gets the port numb of the client
+                byte[] buf = new byte[512];
+                int blockNumber = 1;
+                int bytesRead; // byte reader
+                
+                while ((bytesRead = fis.read(buf)) != -1) {
+
+                    byte[] array = new byte[514]; // byte array for data packets
+                    
+                    array[0] = DATA;
+                    array[1] = (byte) blockNumber;
+
+                    System.out.println(bytesRead);
+                    System.arraycopy(buf, 0, array, 2, buf.length); // copies the content of buf start from 0 then copy arrays content then add the buf.length
+                    DatagramPacket sendPacket = new DatagramPacket(array, array.length, clientAddress, clientPort);
+
+                    if(bytesRead < 512){
+                        sendPacket = new DatagramPacket(array, bytesRead + 2, clientAddress, clientPort);
+                        Retransmit(ds, sendPacket, blockNumber); // Send DATA packet
+                        break;
+                    } else{
+                        Retransmit(ds, sendPacket, blockNumber); // Send DATA packet
+                    }
+                    System.out.println("Sent block #" + blockNumber);
+                    blockNumber++;
+                }
+                System.out.println("File transmitted");
+
+            } catch (IOException e) {
+                System.err.println("Input and Output error: " + e);
+            }
+        } catch (Exception e) {
+
         }
-    }catch(Exception e){
-
-    }
         return;
     }
 
-    private boolean Retransmit(DatagramSocket ds, DatagramPacket dp, int blockNumber){
+    private boolean Retransmit(DatagramSocket ds, DatagramPacket dp, int blockNumber)
+    {
         int attempts = 0;
 
-        System.out.println("recieved ack " + blockNumber);
-        try{
-        ds.setSoTimeout(1000);
+        System.out.println("received ack " + blockNumber);
+        try {
+            ds.setSoTimeout(1000);
 
-        while (attempts < 5) {
-            ds.send(dp); // Send DATA packet
+            while (attempts < 5) {
+                ds.send(dp); // Send DATA packet
 
-            // Receive the ACK
-            byte[] ackBuffer = new byte[2]; // Buffer to receive the ACK
-            DatagramPacket ackPacket = new DatagramPacket(ackBuffer, ackBuffer.length);
-            ds.receive(ackPacket); // Block until ACK is received
+                // Receive the ACK
+                byte[] ackBuffer = new byte[2]; // Buffer to receive the ACK
+                DatagramPacket ackPacket = new DatagramPacket(ackBuffer, ackBuffer.length);
+                ds.receive(ackPacket); // Block until ACK is received
 
-            if (ackPacket.getLength() == 2 && ackPacket.getData()[0] == ACK && blockNumber == ackPacket.getData()[1]) {
-                return true;
+                if (ackPacket.getLength() == 2 && ackPacket.getData()[0] == ACK && blockNumber == ackPacket.getData()[1]) {
+                    return true;
+                }
+
+                attempts++;
             }
-
+        } catch (SocketTimeoutException er) {
             attempts++;
-        }
-        }catch(SocketTimeoutException er){
-            attempts++;
-        }catch(IOException e){
+        } catch (IOException e) {
             System.err.println(e);
             return false;
         }
@@ -101,46 +107,43 @@ class TftpServerWorker extends Thread
 
     public void run()
     {
-
         try {
             byte[] requestData = req.getData();
             if (requestData[0] == RRQ) { // Check if it's an RRQ using the first 2 bytes
-                String filename = new String(requestData, 1, req.getLength() - 1); //making a string constructor extracting file at index 2
+                String filename = new String(requestData, 1, req.getLength() - 1); // making a string constructor extracting file at index 2
                 System.out.println("Received RRQ for file: " + filename);
-                sendfile(filename); //calling the send file method and parsing the file name to it
+                sendfile(filename); // calling the send file method and parsing the file name to it
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    return;
+        return;
     }
 
     public TftpServerWorker(DatagramPacket req)
     {
         this.req = req;
-        //start(); //calling start instead of run directly to create a new thread
+        // start(); //calling start instead of run directly to create a new thread
     }
 }
 
-class TftpServer{
-        
+class TftpServer
+{
     public void start_server()
     {
         try {
             DatagramSocket ds = new DatagramSocket();
-            System.out.println("TftpServer on port " + ds.getLocalPort()); //gets local port number for the datagram socket
+            System.out.println("TftpServer on port " + ds.getLocalPort()); // gets local port number for the datagram socket
 
-            for(;;) {
-            byte[] buf = new byte[1472]; //creating a byte array with a byte limit of 1472 bytes
-            DatagramPacket p = new DatagramPacket(buf, 1472); //creating a datagram packet with the contents of buf(byte array) alongside max number of bytes
-            ds.receive(p); //send the datagram packet to be recived by the datagram socket
-            
-            TftpServerWorker worker = new TftpServerWorker(p); //calling the TftpServerWorker class and parsing the datagram packet on to it
-            worker.start(); //starting the code into the TftpServerWorker
-
+            for (;;) {
+                byte[] buf = new byte[1472]; // creating a byte array with a byte limit of 1472 bytes
+                DatagramPacket p = new DatagramPacket(buf, 1472); // creating a datagram packet with the contents of buf(byte array) alongside max number of bytes
+                ds.receive(p); // send the datagram packet to be received by the datagram socket
+                
+                TftpServerWorker worker = new TftpServerWorker(p); // calling the TftpServerWorker class and parsing the datagram packet on to it
+                worker.start(); // starting the code into the TftpServerWorker
             }
-        }
-        catch(Exception e) { //display error message if an exception is caught
+        } catch (Exception e) { // display error message if an exception is caught
             System.err.println("Exception: " + e);
         }
 
@@ -149,7 +152,7 @@ class TftpServer{
 
     public static void main(String args[])
     {
-    TftpServer d = new TftpServer();
-    d.start_server();
+        TftpServer d = new TftpServer();
+        d.start_server();
     }
 }
